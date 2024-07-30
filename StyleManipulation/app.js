412959c6e9bf85115e2ca6341e40ea01c0fcc2ad
Parva <parvadesai404@gmail.com>
2024-07-30 16:23:29 +0530

    [Add] new questions in style-manipulation ex.

--- StyleManipulation/app.js
+++ StyleManipulation/app.js
@@ -1,17 +1,35 @@
-let listIcons = document.querySelectorAll("div img");
+// let listIcons = document.querySelectorAll("div img");
 
-listIcons[1].setAttribute('src', "css.png");
-listIcons[2].setAttribute('src', "js.png");
+// if (listIcons[1].getAttribute('alt') == "CSS-icon") {
+//     listIcons[1].setAttribute('src', "css.png");
+// }
 
-let listItems = document.querySelectorAll('li');
+// if (listIcons[2].getAttribute('alt') == "JS-icon") {
+//     listIcons[2].setAttribute('src', "js.png");
+// }
 
-listItems[1].classList.add('list-items');
-listItems[2].classList.add('list-items');
+// let listItems = document.querySelectorAll('li');
 
-let h3 = document.querySelectorAll('h3');
-h3[0].classList.add('topic-heading');
+// listItems[1].classList.add('list-items');
+// listItems[2].classList.add('list-items');
 
-h3[1].classList.remove('topic-heading');
+// let h3 = document.querySelectorAll('h3');
+// h3[0].classList.add('topic-heading');
 
-let btn = document.querySelector('button');
-btn.classList.add('btn');
+// h3[1].classList.remove('topic-heading');
+
+// let btn = document.querySelector('button');
+// btn.classList.add('btn');
+
+// let userResponse = prompt("Write 'yes' if you want to enroll, otherwise 'no'");
+
+// if (userResponse == 'no') {
+//     if (!btn.classList.contains('btn-inactive')) {
+//         btn.classList.toggle('btn-inactive');
+//     }
+// }
+// else if (userResponse == 'yes') {
+//     if (!btn.classList.contains('btn-active')) {
+//         btn.classList.toggle('btn-active');
+//     }
+// }
